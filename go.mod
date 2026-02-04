5f2710ff734d9b49c20c37b03208980e39f5245c
Aditya Gupta <adityagupta2087@gmail.com>
2026-02-04 23:06:43 +0530

    reading webcam, writing to file, pending: processing images

---
+++ go.mod
@@ -0,0 +1,7 @@
+module cam
+
+go 1.25.6
+
+require github.com/blackjack/webcam v0.6.1
+
+require golang.org/x/sys v0.14.0 // indirect
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image/jpeg"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/blackjack/webcam"
+)
+
+const BUFFER_SIZE = 12 
+
+func main() { 
+	cam, err := webcam.Open("/dev/video0")
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+	defer cam.Close()
+
+	var format webcam.PixelFormat
+	var framesize webcam.FrameSize
+
+	for f, s := range cam.GetSupportedFormats() {
+		fmt.Printf("format: %s\n", s)
+		format = f
+		break
+	}
+
+	for _, s := range cam.GetSupportedFrameSizes(format) {
+		framesize = s
+		fmt.Printf("framesize: %s\n", framesize.GetString())
+		break
+	}
+
+	_, _, _, err = cam.SetImageFormat(format, framesize.MaxWidth, framesize.MinHeight)
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+
+	err = cam.StartStreaming()
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go readWebCam(signalChan, cam)
+
+	<-signalChan
+}
+
+func readWebCam(signalChan chan os.Signal, cam *webcam.Webcam) {
+	
+	frameChan := make(chan *[]byte, 12)
+
+	go writeToFile(frameChan)
+	go processImage(frameChan)
+
+	for {
+		select {
+		case <-signalChan:
+			close(frameChan)
+			log.Printf("Closing everything now...")
+			os.Exit(0)
+		default:
+			err := cam.WaitForFrame(5)
+			if err != nil { 
+				log.Fatalf("error: %s\n", err)
+			}
+
+			frame, err := cam.ReadFrame()
+			if err != nil { 
+				log.Fatalf("error: %s\n", err)
+			}
+
+			frameChan <- &frame
+		}
+	}
+}
+
+func processImage(frameChn chan *[]byte) {
+	for frame := range frameChn { 
+		reader := bytes.NewReader(*frame)
+		img, err := jpeg.Decode(reader)
+		if err != nil { 
+			log.Fatalf("error: %s\n", err)
+		}
+
+		log.Printf("recieved frame: width=%d height=%d\n", img.Bounds().Dx(), img.Bounds().Dy())
+	}
+
+	log.Printf("closed image processing worker\n") 
+}
+
+func writeToFile(frameChn chan *[]byte) { 
+	var i int 
+	for frame := range frameChn { 
+		err := os.WriteFile(fmt.Sprintf("frames/%d.jpg", i), *frame, 0644)
+		if err != nil { 
+			log.Fatalf("error: %s\n", err)
+		}
+
+		i += 1 
+	}
+
+	log.Printf("closed file writing worker\n")
+}
